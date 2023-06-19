efa2c44c4627bbe6927340f052b9c88f818ae4e5
sathish19961412 <sathishfreelanc5@gmail.com>
2023-06-19 17:17:39 +0530

    npm init in mern stack development

--- frontend/App.js
+++
@@ -1,7 +0,0 @@
-import Headers from "./index"
-
-
-
-export default function App(){
-    <Headers/>
-}
--- frontend/index.js
+++
@@ -1,5 +0,0 @@
-export default function Headers(){
-    return(
-        <h2>welcome To The Frontend</h2>
-    )
-}
